f83f68e17ef37db9ef240b4a75d31a982e2dfee8
Krofficial <liamkrohn@gmail.com>
2022-11-23 09:29:31 -0500

    added additional rooms and room limit

--- Charpie-Game/Assets/Scripts/RoomTemplates.cs
+++ Charpie-Game/Assets/Scripts/RoomTemplates.cs
@@ -10,7 +10,33 @@ public class RoomTemplates : MonoBehaviour {
     public GameObject[] rightRooms;
 
     public GameObject closedRoom;
+    public GameObject startRoom;
 
     public List<GameObject> rooms;
+
+    public float waitTimeSet;
+    private float waitTime;
+
+    private void Start()
+    {
+        waitTime = waitTimeSet;
+    }
+
+
+    private void Update(){
+        if(waitTime <= 0){
+            if (rooms.Count > 40){
+                for (int i = 0; i < rooms.Count; i++){
+                    Destroy(rooms[i]);
+                    // remove index from list *********************
+                }
+                waitTime = waitTimeSet;
+                Instantiate(startRoom, transform.position, Quaternion.identity);
+            }
+        }
+        else{
+            waitTime -= Time.deltaTime;
+        }
+    }
 }
 
