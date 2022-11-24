3b78ea663b4459e7282f6b0ea8ba351893a645b5
Krofficial <liamkrohn@gmail.com>
2022-11-24 02:11:17 -0500

    Finished with the boundaries of the dungeon generation

--- Charpie-Game/Assets/Scripts/RoomTemplates.cs
+++ Charpie-Game/Assets/Scripts/RoomTemplates.cs
@@ -25,13 +25,31 @@ public class RoomTemplates : MonoBehaviour {
 
     private void Update(){
         if(waitTime <= 0){
-            if (rooms.Count > 40){
-                for (int i = 0; i < rooms.Count; i++){
+            if (rooms.Count > 30)
+            {
+                for (int i = 0; i < rooms.Count; i++)
+                {
                     Destroy(rooms[i]);
                     // remove index from list *********************
                 }
                 waitTime = waitTimeSet;
                 Instantiate(startRoom, transform.position, Quaternion.identity);
+                rooms.Clear();
+                Debug.Log("Too Big");
+            }
+            else if (rooms.Count < 15)
+            {
+                for (int i = 0; i < rooms.Count; i++)
+                {
+                    Destroy(rooms[i]);
+                    // remove index from list *********************
+                }
+                waitTime = waitTimeSet;
+                Instantiate(startRoom, transform.position, Quaternion.identity);
+                rooms.Clear();
+                Debug.Log("Too Small");
+
+
             }
         }
         else{
