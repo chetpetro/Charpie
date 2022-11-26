961943c7935c8ac2e6749be5de01d4c8646e64de
Krofficial <liamkrohn@gmail.com>
2022-11-26 14:59:58 -0500

    Added collision and bullet bounce
    collision needs slight revision there are tiny gaps in between rooms but the player can not go through them.

--- Charpie-Game/Assets/Scripts/Bullet.cs
+++ Charpie-Game/Assets/Scripts/Bullet.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Rigidbody2D Rb;
+    private Vector3 lastVelocity;
+    private int bounceCount;
+
+    private void Awake()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        lastVelocity = Rb.velocity;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);//get rid of this single destroy
-        //This is where you check if hitting thing
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            var speed = lastVelocity.magnitude;
+            var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+            Rb.velocity = direction * Mathf.Max(speed, 0f);
+            bounceCount += 1;
+
+            if (bounceCount > 5)
+            {
+                Destroy(gameObject);
+            }
+        }
+
     }
 }
