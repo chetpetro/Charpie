a0a218ed236bb65c4e69f56194fd21924f6f5ffb
Krofficial <liamkrohn@gmail.com>
2022-11-29 21:26:21 -0500

    I added alot of things i can talk about in class

--- Charpie-Game/Assets/Scripts/RoomEntry.cs
+++ Charpie-Game/Assets/Scripts/RoomEntry.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class RoomEntry : MonoBehaviour
 {
     public GameObject doors;
+    public GameObject trigger;
+    public GameObject mask;
+    public int enemyCount;
+    private bool beenTriggered = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
             doors.SetActive(true);
+            mask.SetActive(false);
+            beenTriggered= true;
         }
 
+    }
+
+    private void Update()
+    {
+        if(enemyCount < 1)
+        {
+            doors.SetActive(false);
+            if (beenTriggered)
+            {
+                trigger.SetActive(false);
+            }
+
+        }
     }
 }
