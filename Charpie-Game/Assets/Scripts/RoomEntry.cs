97e191224b0d912401058b6ba322597a7fa90e63
Chet Petro <chet.mh.petro@gmail.com>
2022-11-30 23:10:37 -0500

    Enemy Spawning

--- Charpie-Game/Assets/Scripts/RoomEntry.cs
+++ Charpie-Game/Assets/Scripts/RoomEntry.cs
@@ -7,9 +7,23 @@ public class RoomEntry : MonoBehaviour
     public GameObject doors;
     public GameObject trigger;
     public GameObject mask;
-    private EnemySpawnPoints enemySpawnPoints;
+    public int enemynumber;
     private bool beenTriggered = false;
     private bool enemiesSpawned = false;
+
+    public GameObject[] enemyPoints;
+    public GameObject[] enemyList;
+    public int enemyCount;
+
+    public void spawnEnemies() {
+        enemyCount = Random.Range(4, 9);
+        for (int i = 0; i < enemyCount; i++) {
+            int spawnIndex = Random.Range(0, 16);
+            int enemyIndex = Random.Range(0, enemyList.Length);
+            Debug.Log("Spawned");
+            Instantiate(enemyList[enemyIndex], enemyPoints[spawnIndex].transform.position, enemyPoints[spawnIndex].transform.rotation);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -19,19 +33,22 @@ public class RoomEntry : MonoBehaviour
             mask.SetActive(false);
             beenTriggered= true;
 
-            if (!enemiesSpawned)
-            {
-                enemySpawnPoints.spawnEnemies();
+            if (enemyPoints.Length == 0) {
+                enemiesSpawned= true;
+            }
+
+            if (!enemiesSpawned) {
+                spawnEnemies();
                 enemiesSpawned = true;
             }
-            
+
         }
 
     }
 
     private void Update()
     {
-        if(enemySpawnPoints.enemyCount < 1)
+        if(enemyCount < 1)
         {
             doors.SetActive(false);
             if (beenTriggered)
@@ -41,4 +58,5 @@ public class RoomEntry : MonoBehaviour
 
         }
     }
+
 }
