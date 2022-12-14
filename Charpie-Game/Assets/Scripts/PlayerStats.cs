f3ee345398029a52fc4acee17a28b6cc346e6e1a
Krofficial <liamkrohn@gmail.com>
2022-12-14 09:29:48 -0500

    adkhaldhalfhahgfshg;aghuow;

--- Charpie-Game/Assets/Scripts/PlayerStats.cs
+++ Charpie-Game/Assets/Scripts/PlayerStats.cs
@@ -7,9 +7,16 @@ public class PlayerStats : MonoBehaviour
 {
     public int playerHeath;
     public int playerCoins;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        healthBar.SetMaxHealth(playerHeath);
+    }
 
     private void Update()
     {
+        healthBar.SetHealth(playerHeath);
         if(playerHeath <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
