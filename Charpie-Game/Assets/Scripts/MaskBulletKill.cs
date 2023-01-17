6f9fd1cf1089882ef59ddb9c36a090bdb387e986
Krofficial <liamkrohn@gmail.com>
2023-01-17 09:28:08 -0500

    vagkhdhkafc

---
+++ Charpie-Game/Assets/Scripts/MaskBulletKill.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskBulletKill : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bullet")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Charpie-Game/Assets/Scripts/UpdateGraph.cs
+++ Charpie-Game/Assets/Scripts/UpdateGraph.cs
@@ -8,6 +8,15 @@ public class UpdateGraph : MonoBehaviour
     public float waitTime = 3f;
     public bool updated = false;
 
+    private RoomTemplates roomTemplates;
+    public GameObject door;
+
+
+    private void Start()
+    {
+        roomTemplates = FindObjectOfType<RoomTemplates>();
+    }
+
 
     // After the map has generated, update the graphs of the map so the A* pathfinding algroithm can work
     void Update(){
@@ -15,6 +24,16 @@ public class UpdateGraph : MonoBehaviour
             if (!updated){
                 AstarPath.active.Scan();
                 updated = true;
+
+
+                for(int i = 1; i < roomTemplates.rooms.Count; i++)
+                {
+                    if(roomTemplates.rooms[roomTemplates.rooms.Count - i].name != "Empty Room (Clone)")
+                    {
+                        Instantiate(door, roomTemplates.rooms[roomTemplates.rooms.Count - i].transform.position, roomTemplates.rooms[roomTemplates.rooms.Count - i].transform.rotation);
+                        break;
+                    }
+                }
             }
         }
         else{
