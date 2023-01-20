656f66f32fccbd81745e011fbae349ac6a3c64ff
Krofficial <liamkrohn@gmail.com>
2023-01-20 09:29:00 -0500

    cloud

---
+++ Charpie-Game/Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    //This is a variable that both clouds have that determines their movement speed
+    public float cloudRate;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This moves the clouds
+        gameObject.transform.position = new Vector3 (gameObject.transform.position.x + cloudRate, gameObject.transform.position.y, gameObject.transform.position.z );
+
+        //This brings the clouds back to the left after they go off screen
+        if (gameObject.transform.position.x >= 2500)
+        {
+            gameObject.transform.position = new Vector3(-100, Random.Range(600, 700), gameObject.transform.position.z);
+       
+        }
+    }
+}
