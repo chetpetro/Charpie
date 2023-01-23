7f5213b2962609c5f1117e3b402e70996a39fef1
Krofficial <liamkrohn@gmail.com>
2023-01-23 09:03:26 -0500

    coldbg hjhmhk,umghghghgngn

--- Charpie-Game/Assets/Scripts/CloudMovement.cs
+++ Charpie-Game/Assets/Scripts/CloudMovement.cs
@@ -17,9 +17,9 @@ public class CloudMovement : MonoBehaviour
         gameObject.transform.position = new Vector3 (gameObject.transform.position.x + cloudRate, gameObject.transform.position.y, gameObject.transform.position.z );
 
         //This brings the clouds back to the left after they go off screen
-        if (gameObject.transform.position.x >= 2500)
+        if (gameObject.transform.position.x >= 2600)
         {
-            gameObject.transform.position = new Vector3(-100, Random.Range(600, 700), gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(Random.Range(-400, -300), Random.Range(600, 700), gameObject.transform.position.z);
        
         }
     }
